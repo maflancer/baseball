dd2da1d5786105c8618086e18080c20c44ce3ac5
Matthew Flancer <matthew@visimo.ai>
2024-05-14 12:55:40 -0400

    Add unit tests using vitest

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   base: "/baseball/",
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setupTest.js",
+  },
 });
